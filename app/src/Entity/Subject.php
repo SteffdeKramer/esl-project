0bfeb8ad1a0136fe3cdd9b65226828d665e81230
SteffdeKramer <stefdekramer3@gmail.com>
2021-12-24 18:01:31 +0100

    adding subjects

---
+++ app/src/Entity/Subject.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubjectRepository::class)]
+class Subject
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'integer')]
+    private $block;
+
+    #[ORM\Column(type: 'integer')]
+    private $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBlock(): ?int
+    {
+        return $this->block;
+    }
+
+    public function setBlock(int $block): self
+    {
+        $this->block = $block;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
